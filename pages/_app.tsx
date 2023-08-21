1d93165447f5c2c352ddb38d5a41077c026533f6
Serhii Zharkov <gregamezzo@gmail.com>
2023-08-21 13:35:26 +0300

    Add GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,12 +15,13 @@ import {
   getDefaultWallets,
   RainbowKitProvider,
 } from "@rainbow-me/rainbowkit";
+import Script from "next/script";
 
 // Setup provider
-const { chains, publicClient } = configureChains([base], [publicProvider()]);
+const {chains, publicClient} = configureChains([base], [publicProvider()]);
 
 // Setup connector
-const { connectors } = getDefaultWallets({
+const {connectors} = getDefaultWallets({
   appName: "FriendSwap",
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID ?? "",
   chains,
@@ -33,13 +34,35 @@ const wagmiConfig = createConfig({
   publicClient,
 });
 
-export default function FriendSwap({ Component, pageProps }: AppProps) {
+export default function FriendSwap({Component, pageProps}: AppProps) {
   return (
-    // Wrap in RainbowKit providers
-    <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains} theme={lightTheme()}>
-        <Component {...pageProps} />
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <>
+      <WagmiConfig config={wagmiConfig}>
+        <RainbowKitProvider chains={chains} theme={lightTheme()}>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
+      {process.env.NODE_ENV === "production" ? (
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      ) : null}
+      {process.env.NODE_ENV === "production" ? (
+        <Script
+          id="analytics"
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+        />
+      ) : null}
+    </>
   );
 }
